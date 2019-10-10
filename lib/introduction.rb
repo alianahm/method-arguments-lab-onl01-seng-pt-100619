3e087a3ac13cb52979d8cc5eb2d632c83d12ba11
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-10 06:47:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,5 @@ def introduction(josh)
 end
 
 def introduction_with_language(josh, ruby)
-  puts "Hi, my name is #{josh}"
+  puts "Hi, my name is #{josh} and I am learning to program in #{ruby}."
+end
